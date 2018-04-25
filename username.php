45a6887d397f219db58f023e086e7e8a748da8c7
Ikechi Tuchi Marvelous <marvelike7@gmail.com>
2018-04-25 14:39:19 +0100

    config file alingned to suit server settings

---
+++ username.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Username</title>
+<style>
+.img
+{
+   align ="center"; 
+}
+</style>
+</head>
+<body>
+   <img class="img" src ="profile.jpg">
+   <h3 class="img">Ikechi Tuchi Marvelous</h3>
+</body>
+</html>
