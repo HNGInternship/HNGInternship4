9422862eaeaf17c2322b0fd53644167452c5e633
unknown <cosmasopurum@gmail.com>
2018-05-12 00:35:00 +0100

    just added my name

---
+++ index.php
@@ -0,0 +1,18 @@
+       <html lang="en-US">
+           <meta name="author" content="Bolaji Ayodeji">
+           <meta name="description" content="HTML5 CSS3">
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+           <link rel="stylesheet" href="style.css">
+           
+           <body> 
+               <br />
+           <h4 id=h4> &nbsp; &nbsp; &nbsp; &nbsp; <bold> HNG INTERNSHIP 4.0 </bold> </h4>
+           <div style="color:#333333 ; text-align:left; font-size:100%";>
+           <br /> <br /> <br />
+           <h4 id="text1">Time:</h4>
+           <?php echo date("h:i:s:A") ;?>
+           <h4 id="text1">Date:</h4>
+           <?php echo date("l:F:dS:Y") ;?>
+            </div>
+           </body>
+       </html>
---
+++ time.php
@@ -0,0 +1,15 @@
+<!DOCTYPE>
+<html lang="em">
+<head>
+        <link href="style.css" type="text/css" rel="stylesheet">
+<title>Time</title>
+</head>
+<body>
+        <h1>HNG INTERNSHIP</h1>
+<?php
+date_default_timezone_set("Africa/Lagos");
+echo "The time is ".date("h:i:sa");;
+?>
+
+</body>
+</html>
